b4f518a207a5db51d0e2866fe0a3a7249856f700
unknown <jey0210@naver.com>
2020-08-30 17:09:53 +0900

    리액트를 이용한 프로젝트입니다.

---
+++ recipe/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  const APP_ID = ""
+  const APP_KEY = ""
+
+  return(
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  )
+}
+export default App;
